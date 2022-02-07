0781d23fb2aa3bb69d88d8af19ee3dac2d034b9b
Ola <ola@olavea.com>
2022-02-07 11:56:30 +0100

    wait with this

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
 // POW!-site/gatsby-node.js
-const ID_GiGGLES = `eRTJPIa39a4`;
-const axios = require("axios");
+// const ID_GiGGLES = `eRTJPIa39a4`;
+// const axios = require("axios");
 const { createFilePath } = require("gatsby-source-filesystem");
 
-async function prepPugNode({ actions, createNodeId, createContentDigest }) {
-  const { data } = await axios.get("https://www.youtube.com/oembed", {
-    params: {
-      url: `https://youtu.be/${ID_GiGGLES}`,
-      maxwidth: 356,
-    },
-  });
-  actions.createNode({
-    ...data,
-    id: createNodeId(`pug-${ID_GiGGLES}`),
-    internal: {
-      contentDigest: createContentDigest(ID_GiGGLES),
-      type: `pugNode`,
-    },
-  });
-}
+// async function prepPugNode({ actions, createNodeId, createContentDigest }) {
+//   const { data } = await axios.get("https://www.youtube.com/oembed", {
+//     params: {
+//       url: `https://youtu.be/${ID_GiGGLES}`,
+//       maxwidth: 356,
+//     },
+//   });
+//   actions.createNode({
+//     ...data,
+//     id: createNodeId(`pug-${ID_GiGGLES}`),
+//     internal: {
+//       contentDigest: createContentDigest(ID_GiGGLES),
+//       type: `pugNode`,
+//     },
+//   });
+// }
 //                 Troya Goatsby and Lilly Owlsby are
 //                 Baking pages with
 //                 Cap'n Granny Sharksby's
@@ -28,14 +28,17 @@ async function bakePOWsitePagesFromMarkdown({ graphql, actions }) {
   //              1. bakingSong = Lilly Owslby sings 🦉
   //                 the bakingSong by Cap'n Granny Sharksby
   const bakingSong = require.resolve("./src/templates/RecipeMarkdown.js");
-  //                 Look for _ _ _ in http://localhost:8000/topping/Duct-Tape 👻
-  //              2. supplies: data from Markdown files 🌲 in  and _ _ _ drawn
+  //                 Look for _ _ _ in http://localhost:8000/_ _ _ /_ _ _  👻
+  //              2. supplies: data from Markdown files
   const { data } = await graphql(`
     {
       supplies: allMarkdownRemark(
         sort: { order: ASC, fields: frontmatter___title }
       ) {
         nodes {
+          fields {
+            slug
+          }
           id
         }
       }
@@ -46,8 +49,8 @@ async function bakePOWsitePagesFromMarkdown({ graphql, actions }) {
   //                 for each bake a page
   data.supplies.nodes.forEach((ahoyGoodie, index) => {
     console.log(
-      "Ginger bread ahoyGoodie! id: ",
-      ahoyGoodie.id,
+      "Ginger bread ahoyGoodie! slug: ",
+      ahoyGoodie.fields.slug,
       "index: ",
       index
     );
@@ -55,7 +58,7 @@ async function bakePOWsitePagesFromMarkdown({ graphql, actions }) {
     actions.createPage({
       //          A. «Ahoy! Goodie?!»
       //             Cap'n Fox shouts and embarks. 🦊
-      path: `${ahoyGoodie.id}`,
+      path: `${ahoyGoodie.fields.slug}`,
       //          B. Owlsby sings badly
       //             and bakes baby sharks. 🦈
       component: bakingSong,
@@ -65,10 +68,9 @@ async function bakePOWsitePagesFromMarkdown({ graphql, actions }) {
         catsby: ahoyGoodie.id,
         fox: "Catsby looks tasty I getz hungry for kitten",
       },
-      //          D. DSG 🎩 Piraty mistakes and sharky bugz
       //          D. Don't Show Goodies to Fox
       //             and maybe get bitten.
-      defer: index + 1 > 2,
+      //defer: index + 1 > 2,
       //              D. Data tree 🤖🌲
       //              E. Every node you must sort 💰
       //              F. Fields Forever `fields: ,` 🍓
@@ -92,23 +94,50 @@ async function prepPOWmarkdownNode({
   });
 }
 
-exports.sourceNodes = async (params) => {
-  await Promise.all([prepPugNode(params)]);
-};
+// exports.sourceNodes = async (params) => {
+//   await Promise.all([prepPugNode(params)]);
+// };
 
-exports.createPages = async (params) => {
-  await Promise.all([bakePOWsitePagesFromMarkdown(params)]);
-};
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
+async function prepPugNode({ node, actions, getNode }) {
   const { createNodeField } = actions;
 
   if (node.internal.type === "MarkdownRemark") {
     const slug = createFilePath({ node, getNode });
+    // https://www.gatsbyjs.com/plugins/gatsby-source-filesystem/?=files#createfilepath
+    createNodeField({
+      // 🐛
+      name: "slug",
+      node,
+      value: slug,
+    });
+  }
+}
+
+// POW!-site/gatsby-node.js
+async function prepMarkdownRemarkNode({ actions, node, getNode }) {
+  // 🔨💰🍓
+  const { createNodeField } = actions;
+  // my md type of node ... internal
+  if (node.internal.type === "MarkdownRemark") {
+    // 🐛 = 🔨 + 📁 + 🎢
+    const slug = createFilePath({ node, getNode });
+    // 🔨💰🍓 ({ 🐛, 💰, Where does this 🐛 slug 🐛 come from 📂?})
     createNodeField({
       name: "slug",
       node,
       value: slug,
     });
   }
+}
+
+exports.onCreateNode = async (params) => {
+  await Promise.all([prepMarkdownRemarkNode(params)]);
 };
+
+exports.createPages = async (params) => {
+  await Promise.all([bakePOWsitePagesFromMarkdown(params)]);
+};
+
+// exports.onCreateNode = async (params) => {
+//   await Promise.all([prepPugNode(params)]);
+// };
