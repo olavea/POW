e64c032f4f420911a41b302f23ead53afac8b18a
Benedicte Raae <raae@raae.codes>
2022-05-04 15:25:51 +0200

    add more process envs

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,18 @@
 const DEPLOY_PRIME_URL = process.env.DEPLOY_PRIME_URL;
+const URL = process.env.URL;
+const DEPLOY_URL = process.env.DEPLOY_URL;
 
 exports.handler = async (event, context) => {
   console.log({ DEPLOY_PRIME_URL });
+  console.log({ URL });
+  console.log({ DEPLOY_URL });
   return {
     statusCode: 200,
-    body: JSON.stringify({ hello: "world", prime_url: DEPLOY_PRIME_URL }),
+    body: JSON.stringify({
+      hello: "world",
+      prime_url: DEPLOY_PRIME_URL,
+      deploy_url: DEPLOY_URL,
+      url: URL,
+    }),
   };
 };
